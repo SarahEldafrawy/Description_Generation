01c815c5d60d389e13d2ac9a79751e5e5b853c7d
kamal <kamalhabieb@yahoo.ca>
2019-07-28 18:56:36 +0200

    All changes + new Voice

--- src/Templates.java
+++ src/Templates.java
@@ -5,61 +5,37 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
-/*
-* intro
-* min
-* max
-* pattern
-* increase
-* decrease
-* steady
-*
-* This {2} deals with the {3} as a relation between {1} and {0} particularly in {4},
-* We can see that the lowest {2} recorded was {0} for {1},
-* and the highest {2} recorded was {0} for {1},
-* looking through the data some certain patterns have been detected {0},
-* over the {1} the {0} has increased by {2},
-* over the {1} the {0} has decreased by {2},
-* There has been no change in {1} over the {0};
-*/
-
-/*
-In the following years the total growth went down to about 250,000 in 1998/99.
-From that time on the Canadian population has been gradually growing again although the natural increase slows down.
-So we can say that the growth of the population in Canada is based on migration.
-*/
-
 
 public class Templates {
-    //todo fillin templates : i:template , j:sentence
     /*
       first sentence:intro
       second sentence:min
       third sentence:max
       fourth sentence:pattern
       fifth sentence:increase/decrease (rate)
-     */
-    private final int intro=0,min=1,max=2,patt=3,inc=4,dec=5,steady=6;
+    */
+    private final int intro=0,min=1,max=2,patt=3,inc=4,dec=5,steady=6,introNoTitle =7;
     private String [][] templates = new String [4][];
     public Templates(){
         createTemplates();
     }
     private void createTemplates(){
-        //todo keep the measure and the dimension from the first sentence to use then
-        templates[0] = new String[7];//j=4:increase, j=5:decrease
-        templates[1] = new String[7];//j=4:increase, j=5:decrease
-        templates[2] = new String[7];//j=4:increase, j=5:decrease
-        templates[0][intro] = "This {2} deals with the {3} as a relation between {1} and {0} particularly in {4}";
-        //todo to put or not to put dim_unit
+        templates[0] = new String[8];//j=4:increase, j=5:decrease
+        templates[1] = new String[8];//j=4:increase, j=5:decrease
+        templates[2] = new String[8];//j=4:increase, j=5:decrease
+        templates[0][intro] = "This {2} deals with the {3} as a relation between {1} and {0}";
         //0:dimension, 1:measure, 2:type, 3:title, 4:dimension_type
+
+        templates[0][introNoTitle] ="This {2} describes a relation between {1} and {0}";
+        //0:dimension, 1:measure
 
         templates[0][min] = ",We can see that the lowest {2} recorded was {0} for {1}";
         //0:measure_min_value, 1:dimension_min, 2:measure_min
 
-        templates[0][max] = ",and the highest {2} recorded was {0} for {1}";//todo to put or not to put meas_unit
+        templates[0][max] = ",and the highest {2} recorded was {0} for {1}";
         //0:measure_max_value, 1:dimension_max, 2:measure_max
 
-        templates[0][patt] = ",looking through the data some certain pattern{1} have been detected: {0}";
+        templates[0][patt] = ",looking through the data some pattern{1} been detected: {0}";
         //0:the patterns
 
         templates[0][inc] = ",looking through the data it was found the {0} is increasing.";
@@ -71,17 +47,18 @@ public class Templates {
         templates[0][steady] = ",There has been no change the {0}.";
         //0:measure
 
-        templates[1][intro] = "the {2} shows the {3} in " +
-                "{0} of {4} {1} for the dimension_value {0}"; //todo to put or not to put dim_unit
+        templates[1][intro] = "the {1} for the {0} is represented by this {2} to describe the {3}";
         //0:dimension, 1:measure, 2:type, 3:title, 4:dimension_type
+
+        templates[1][introNoTitle] = "the {1} for the {0} is represented by this {2}";
 
         templates[1][min] = ",the lowest {2} is about {0} for {1}";
         //0:measure_min_value, 1:dimension_min, 2:measure_min
 
-        templates[1][max] = ",the highest {2} is about {0} for {1}";//todo to put or not to put meas_unit
+        templates[1][max] = ",the highest {2} is about {0} for {1}";
         //0:measure_max_value, 1:dimension_max, 2:measure_max
 
-        templates[1][patt] = ",the following pattern{1} have been observed: {0}";
+        templates[1][patt] = ",the following pattern{1} been observed: {0}";
         //0:the patterns
 
         templates[1][inc] = ",the numbers show that the {0} is increasing.";
@@ -94,16 +71,18 @@ public class Templates {
         //0:measure
 
         templates[2][intro] = "the {2} illustrates the {3} in " +
-                "{0} of {4} {1} for the dimension_value {0}"; //todo to put or not to put dim_unit
+                "{1} by {0}";
         //0:dimension, 1:measure, 2:type, 3:title, 4:dimension_type
+        templates[2][introNoTitle] = "the {2} illustrates the relation between " +
+                "{1} and {0}";
 
         templates[2][min] = ",the least {2} is about {0} for {1}";
         //0:measure_min_value, 1:dimension_min, 2:measure_min
 
-        templates[2][max] = ",the most {2} is about {0} for {1}";//todo to put or not to put meas_unit
+        templates[2][max] = ",the most {2} is about {0} for {1}";
         //0:measure_max_value, 1:dimension_max, 2:measure_max
 
-        templates[2][patt] = ",the following pattern{1} have been observed: {0}";
+        templates[2][patt] = ",the following pattern{1} been observed: {0}";
         //0:the patterns
 
         templates[2][inc] = ",{0} is found to be increasing";
@@ -179,7 +158,6 @@ public class Templates {
 
         for(int index=0;index<patterns.size();index++){
             Pair pattern = patterns.get(index);
-            //todo add measure unit
             allPatterns.append(pattern.getKey());
             allPatterns.append("have about the same ");
             allPatterns.append(measure);
@@ -187,9 +165,14 @@ public class Templates {
             allPatterns.append(adjustPercision((Double) pattern.getValue()));
         }
 
-
-        firstSentence = MessageFormat.format(firstSentence,dimension,measure,
-                firstSenMap.get("type"), firstSenMap.get("title"),firstSenMap.get("dimension_types"));
+        if(fifthSenMap.containsKey("title")){
+            firstSentence = MessageFormat.format(firstSentence,dimension,measure,
+                    firstSenMap.get("type"), firstSenMap.get("title"));
+        }
+        else{
+            firstSentence = MessageFormat.format(firstSentence,dimension,measure,
+                    firstSenMap.get("type"));
+        }
 
         secondSentence = MessageFormat.format(secondSentence,adjustPercisionS(secondSenMap.get("measure_max_value")),
                 secondSenMap.get("dimension_max"),secondSenMap.get("measure_max").replaceAll("\"",""));
@@ -198,10 +181,10 @@ public class Templates {
                 thirdSenMap.get("dimension_min"),thirdSenMap.get("measure_min").replaceAll("\"",""));
 
         if(patterns.size()>1){
-            fourthSentence = MessageFormat.format(fourthSentence,allPatterns.toString(),"s");
+            fourthSentence = MessageFormat.format(fourthSentence,allPatterns.toString(),"s have");
         }
         else
-            fourthSentence = MessageFormat.format(fourthSentence,allPatterns.toString(),"");
+            fourthSentence = MessageFormat.format(fourthSentence,allPatterns.toString()," has");
 
         if(fifthKey.equals("steady")){
             fifthSentence = MessageFormat.format(fifthSentence,measure);
